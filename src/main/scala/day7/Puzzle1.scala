d3425d02a56728792039f2fc901814f3eb29c4cc
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-12-07 20:03:35 +0100

    Day 7 code cleanup.

--- src/main/scala/day7/Puzzle1.scala
+++ src/main/scala/day7/Puzzle1.scala
@@ -3,6 +3,8 @@ package day7
 import util.{FileReader, InputParser}
 import util.InputParser._
 
+import scala.math.abs
+
 object Puzzle1InputParser extends InputParser[Seq[Int]] {
   override def parse(string: String): Seq[Int] =
     string.splitBy(",").map(_.toInt)
@@ -11,14 +13,18 @@ object Puzzle1InputParser extends InputParser[Seq[Int]] {
 object Puzzle1 extends App {
   val input = FileReader.readUnsafe("input/day7/puzzle1.txt")
   val positions = Puzzle1InputParser.parse(input)
-  val positionToCountMap = positions.groupBy(identity).view.mapValues(_.length)
-  val (minPosition, maxPosition) = positionToCountMap.keys.foldLeft((Int.MaxValue, Int.MinValue)) {
+  val (minPosition, maxPosition) = positions.foldLeft((Int.MaxValue, Int.MinValue)) {
     case ((min, max), position) => (position.min(min), position.max(max))
   }
-  val costs = LazyList.range(minPosition, maxPosition + 1).map { finalPosition =>
+  val positionToCountMap = positions.groupBy(identity).view.mapValues(_.length)
+  val minCost = (minPosition to maxPosition).map(totalFuelCost).min
+  println(minCost)
+
+  private def totalFuelCost(finalPosition: Int) =
     positionToCountMap.map {
-      case (position, count) => math.abs(finalPosition - position) * count
+      case (position, count) => fuelCost(position, finalPosition) * count
     }.sum
-  }
-  println(costs.min)
+
+  private def fuelCost(position: Int, finalPosition: Int) =
+    abs(finalPosition - position)
 }
--- src/main/scala/day7/Puzzle2.scala
+++ src/main/scala/day7/Puzzle2.scala
@@ -3,6 +3,8 @@ package day7
 import util.{FileReader, InputParser}
 import util.InputParser._
 
+import scala.math.abs
+
 object Puzzle2InputParser extends InputParser[Seq[Int]] {
   override def parse(string: String): Seq[Int] =
     string.splitBy(",").map(_.toInt)
@@ -11,16 +13,20 @@ object Puzzle2InputParser extends InputParser[Seq[Int]] {
 object Puzzle2 extends App {
   val input = FileReader.readUnsafe("input/day7/puzzle2.txt")
   val positions = Puzzle2InputParser.parse(input)
-  val positionToCountMap = positions.groupBy(identity).view.mapValues(_.length)
-  val (minPosition, maxPosition) = positionToCountMap.keys.foldLeft((Int.MaxValue, Int.MinValue)) {
+  val (minPosition, maxPosition) = positions.foldLeft((Int.MaxValue, Int.MinValue)) {
     case ((min, max), position) => (position.min(min), position.max(max))
   }
-  val costs = LazyList.range(minPosition, maxPosition + 1).map { finalPosition =>
+  val positionToCountMap = positions.groupBy(identity).view.mapValues(_.length)
+  val minCost = (minPosition to maxPosition).map(totalFuelCost).min
+  println(minCost)
+
+  private def totalFuelCost(finalPosition: Int) =
     positionToCountMap.map {
-      case (position, count) =>
-        val distance = math.abs(finalPosition - position)
-        distance * (distance + 1) / 2 * count
+      case (position, count) => fuelCost(position, finalPosition) * count
     }.sum
+
+  private def fuelCost(position: Int, finalPosition: Int) = {
+    val distance = abs(finalPosition - position)
+    distance * (distance + 1) / 2
   }
-  println(costs.min)
 }
