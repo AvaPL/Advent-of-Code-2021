f9077f2d52f5b44825ac6f7c4419bcee10f8b338
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-12-25 12:16:51 +0100

    Finished day 21 puzzle 2.

--- src/main/scala/day21/Puzzle2.scala
+++ src/main/scala/day21/Puzzle2.scala
@@ -2,6 +2,8 @@ package day21
 
 import day21.Game.winningScore
 
+import scala.collection.mutable
+
 case class Player(position: Int, score: Int = 0) {
   def moveTo(newPosition: Int): Player =
     copy(position = newPosition, score = score + newPosition)
@@ -19,17 +21,35 @@ case object Player2Turn extends Turn {
   override def next: Turn = Player1Turn
 }
 
-case class Game(p1: Player, p2: Player, currentTurn: Turn = Player1Turn) {
+case class Game(
+                 p1: Player,
+                 p2: Player,
+                 currentTurn: Turn = Player1Turn,
+                 private val rollsCount: Int = 0,
+                 private val dieScore: Int = 0
+               ) {
 
-  def rollDie(dieScore: Int): Game =
-    currentTurn match {
-      case Player1Turn =>
-        val position = calculatePosition(p1.position, dieScore)
-        copy(p1 = p1.moveTo(position), currentTurn = currentTurn.next)
-      case Player2Turn =>
-        val position = calculatePosition(p2.position, dieScore)
-        copy(p2 = p2.moveTo(position), currentTurn = currentTurn.next)
-    }
+  def rollDie(rolledScore: Int): Game = {
+    val newRollsCount = rollsCount + 1
+    val newDieScore = dieScore + rolledScore
+    if (newRollsCount < 3)
+      copy(rollsCount = newRollsCount, dieScore = newDieScore)
+    else
+      currentTurn match {
+        case Player1Turn => movePlayer1(newDieScore)
+        case Player2Turn => movePlayer2(newDieScore)
+      }
+  }
+
+  private def movePlayer1(newDieScore: Int) = {
+    val position = calculatePosition(p1.position, newDieScore)
+    copy(
+      p1 = p1.moveTo(position),
+      currentTurn = currentTurn.next,
+      rollsCount = 0,
+      dieScore = 0
+    )
+  }
 
   private def calculatePosition(position: Int, dieScore: Int) = {
     val newPosition = position + dieScore
@@ -37,6 +57,16 @@ case class Game(p1: Player, p2: Player, currentTurn: Turn = Player1Turn) {
       10
     else
       newPosition % 10
+  }
+
+  private def movePlayer2(newDieScore: Int) = {
+    val position = calculatePosition(p2.position, newDieScore)
+    copy(
+      p2 = p2.moveTo(position),
+      currentTurn = currentTurn.next,
+      rollsCount = 0,
+      dieScore = 0
+    )
   }
 
   def hasGameFinished: Boolean =
@@ -48,24 +78,31 @@ object Game {
 }
 
 object Puzzle2 extends App {
-  val p1StartPosition = 4 // TODO: Replace with input
-  val p2StartPosition = 8 // TODO: Replace with input
-  val dieResults = List(1, 2, 3)
+  val p1StartPosition = 3
+  val p2StartPosition = 7
+  val dieResults = 1 to 3
   val game = Game(Player(p1StartPosition), Player(p2StartPosition))
   val (p1Wins, p2Wins) = countWinningUniverses(game)
-  println(s"p1Wins = $p1Wins")
-  println(s"p2Wins = $p2Wins")
+  val mostWins = p1Wins.max(p2Wins)
+  println(mostWins)
 
-  private def countWinningUniverses(game: Game, p1Wins: Long = 0, p2Wins: Long = 0): (Long, Long) = {
-    if (game.hasGameFinished)
-      game.currentTurn match {
-        case Player1Turn => (p1Wins, p2Wins + 1)
-        case Player2Turn => (p1Wins + 1, p2Wins)
+  private def countWinningUniverses(game: Game) = {
+    val cache = mutable.Map[Game, (Long, Long)]()
+
+    def loop(game: Game): (Long, Long) = {
+      if (game.hasGameFinished)
+        game.currentTurn match {
+          case Player1Turn => (0, 1)
+          case Player2Turn => (1, 0)
+        }
+      else {
+        cache.getOrElseUpdate(game, {
+          val (p1Wins, p2Wins) = dieResults.map(game.rollDie).map(loop).unzip
+          (p1Wins.sum, p2Wins.sum)
+        })
       }
-    else {
-      // TODO: Roll die 3 times per turn
-      val (allP1Wins, allP2Wins) = dieResults.map(game.rollDie).map(countWinningUniverses(_, p1Wins, p2Wins)).unzip
-      (allP1Wins.sum, allP2Wins.sum)
     }
+
+    loop(game)
   }
 }
